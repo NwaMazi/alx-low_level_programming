33a684ca93bacd8af1a0e2a28c24764c03334331
NwaMazi <nwamazi1785@gmail.com>
2023-06-15 19:29:01 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *  main - this program displays the size of various types.
+ *  Return: 0
+ */
+int main(void)
+{
+printf("size of int is %d bytes \n", sizeof(int));
+printf("size of float is %d bytes \n", sizeof(float));
+printf("size of char is %d bytes \n", sizeof(char));
+printf("size of double is %d bytes \n", sizeof(double));
+return (0);
+}
