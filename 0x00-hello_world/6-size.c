4a8c4021a34de798e8c70c62eac4a808b15c3178
NwaMazi <nwamazi1785@gmail.com>
2023-06-15 19:33:18 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,7 @@ int main(void)
 printf("size of int is %d bytes \n", sizeof(int));
 printf("size of float is %d bytes \n", sizeof(float));
 printf("size of char is %d bytes \n", sizeof(char));
-printf("size of double is %d bytes \n", sizeof(double));
+printf("size of long int is %d bytes \n", sizeof(long int));
+printf("size of long long int is %d bytes \n", sizeof(long long int));
 return (0);
 }
