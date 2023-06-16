3d3dd051d3cc2fba15d80f45f643f0737bdff9c5
NwaMazi <nwamazi1785@gmail.com>
2023-06-15 18:45:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ *  main - this program print displays a comment using printf function
+ *  Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
