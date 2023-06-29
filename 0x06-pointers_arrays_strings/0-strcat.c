91193f7071c7ebc0d279229bdfc49be15eacd2ba
NwaMazi <nwamazi1785@gmail.com>
2023-06-29 16:02:03 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * char *_strcat - this program concatinates but appends two strings.
+ * @dest: destination
+ * @src: origin
+ * Return: destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
