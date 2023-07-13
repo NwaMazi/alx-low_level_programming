ff883189500d1663723aec5c217caab295b72849
NwaMazi <nwamazi1785@gmail.com>
2023-07-12 21:03:05 -0700

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,33 +9,33 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int x, xi;
+	int x, ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	x = xi = 0;
+	x = ci = 0;
 	while (s1[x] != '\0')
 		x++;
-	while (s2[xi] != '\0')
-		xi++;
-	conct = malloc(sizeof(char) * (x + xi + 1));
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (x + ci + 1));
 
 	if (conct == NULL)
 		return (NULL);
-	x = xi = 0;
+	x = ci = 0;
 	while (s1[x] != '\0')
 	{
 		conct[x] = s1[x];
 		x++;
 	}
 
-	while (s2[xi] != '\0')
+	while (s2[ci] != '\0')
 	{
-		conct[x] = s2[xi];
-		x++, xi++;
+		conct[x] = s2[ci];
+		x++, ci++;
 	}
 	conct[x] = '\0';
 	return (conct);
