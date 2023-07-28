99a49c3d6fdd5e2bc68bd53cc4fb6ecd568ff90d
NwaMazi <nwamazi1785@gmail.com>
2023-07-27 19:57:56 -0700

    updated0

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int lent = 0;
+	unsigned int len = 0;
 
-	while (str[lent])
-		lent++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->lent = lent;
+	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,17 +13,17 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	unsigned int lent = 0;
+	unsigned int len = 0;
 
-	while (str[lent])
-		lent++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->lent = lent;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
